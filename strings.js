96d7e61ded250961577e13049407e59eb9ed999d
Pushpak Paunikar <54863992+pushpak00@users.noreply.github.com>
2025-01-29 18:57:24 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,35 @@
+const name = "roman"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('roman-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://roman.com/roman%20wwe"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('lesnor'))
+
+console.log(gameName.split('-'));
